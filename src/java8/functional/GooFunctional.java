70994487b8a247b277e93054aaf9421eff069e34
elliot <elliot.rourke@gmail.com>
2021-05-20 15:39:05 +0100

    Updating functionalDigest items

--- src/java8/functional/GooFunctional.java
+++ src/java8/functional/GooFunctional.java
@@ -3,4 +3,7 @@ package java8.functional;
 @FunctionalInterface
 public interface GooFunctional {
     String doSomethingAwesome(String s);
+    default void mySecondDefault() {
+        System.out.println("Defaulting!");
+    }
 }
